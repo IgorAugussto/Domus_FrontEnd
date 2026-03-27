a992b236b88023c08ad6cf6a1f2f107ca1396d7e
Igor Augusto <igor-augustofc@hotmail.com>
2026-03-27 01:17:32 -0300

    Tentativa de corrigir erro no momento de logar

--- domus-frontend/src/components/LoginPage.tsx
+++ domus-frontend/src/components/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Button } from "../ui-components/button";
 import { Input } from "../ui-components/input";
 import { Label } from "../ui-components/label";
