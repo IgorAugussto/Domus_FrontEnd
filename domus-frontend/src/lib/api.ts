b336be4680d8288aef05cd83f325a8c4c8354727
IgorAugussto <igor-augustofc@hotmail.com>
2026-05-21 17:50:30 -0300

    Atualizando a api.ts para não acontecer erro de looping no momento do login

--- domus-frontend/src/lib/api.ts
+++ domus-frontend/src/lib/api.ts
@@ -14,7 +14,9 @@ api.interceptors.response.use(
     const config = error.config;
 
     if (error.response?.status === 401 || error.response?.status === 403) {
-      window.location.href = '/login';
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
       return Promise.reject(error);
     }
 
