feb2d38e99fd8dc4f68a7d3eee9a77b1c142a835
Igor Augusto <igor-augustofc@hotmail.com>
2026-06-25 20:20:04 -0300

    Mudando endpoint de importação para ir direto para o EC2

--- domus-frontend/src/service/statementService.ts
+++ domus-frontend/src/service/statementService.ts
@@ -34,7 +34,10 @@ export const statementService = {
     formData.append('file', file);
     formData.append('dueDate', dueDate);
 
-    const response = await api.post('/statement/import', formData);
+    const response = await api.post(
+      'https://api.domusapp.dev.br:8443/api/statement/import',
+      formData
+    );
     return response.data; // { jobId: "uuid" }
   },
 
@@ -46,7 +49,9 @@ export const statementService = {
    * result: preenchido apenas quando status === "DONE"
    */
   getStatus: async (jobId: string): Promise<StatementJobStatus> => {
-    const response = await api.get(`/statement/status/${jobId}`);
+    const response = await api.get(
+      `https://api.domusapp.dev.br:8443/api/statement/status/${jobId}`
+    );
     return response.data;
   },
 };
